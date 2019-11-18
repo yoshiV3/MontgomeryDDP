06e2c73ec206204f5679d5ebb8ceed3e3a14d529
Jonas Bertels <r0666113@pc-klas4-12.esat.kuleuven.be>
2019-11-18 16:55:26 +0100

    Take that

--- rtl_tb/tb_adder.v
+++ rtl_tb/tb_adder.v
@@ -102,13 +102,13 @@ module tb_adder();
     /*************TEST ADDITION*************/
     
     $display("\nAddition with testvector 1");
-    assign subtractNumber = 512'hf;
-    // Check if 1+1=2
+    assign subtractNumber = 512'h9affa8a49591a576f49884b7b29a9ba53ebc4856ea3a49267d2ab2fc0cd8e318f56955d66d56dc98f59f69d4ae8eef93280e5018ad303fb36cd38836a3ab0af2;
+    // Check if 1+1=2 //
     #`CLK_PERIOD;
     assign number1 = 514'b11011001110110100111101111101010000110100011000111011000101010111110001010100010011110110100111010000101010111000101110001011100010100001110110100000000110001001000001110001000111010101001101100001111101101111100001000000100110000101100000100101101001110011001011100010101011110100110111111001000111001001011101111100100001100101100010000001101001101011111001001110001011000001001001011101011101000000010111000110111100110000001011111010110001101101010000101000100010101010001110111110100100110101101111000110111;
     assign expected = 6 + number1 + number1+number1+1; //
     assign expected = {1'b0,expected[513:1]};
-    assign expected = expected - 1 - subtractNumber;
+    assign expected = expected - 4*(subtractNumber);
     perform_add(514'd3);
     perform_add(514'd3);
     perform_add(514'd0);
@@ -133,7 +133,8 @@ module tb_adder();
     #`CLK_PERIOD
     
     subtract <= 1'b1;
-    in_a <= 512'hfffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff0;
+    //in_a <= 512'hfffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffdfffff0;
+    in_a <= ~subtractNumber + 1;
     return  <= 4'd0;
     #`CLK_PERIOD
     return  <= 4'd1;
@@ -144,6 +145,39 @@ module tb_adder();
     #`CLK_PERIOD
     return  <= 4'd4;
     #`CLK_PERIOD
+        return  <= 4'd0;
+    #`CLK_PERIOD
+    return  <= 4'd1;
+    #`CLK_PERIOD
+    return  <= 4'd2;
+    #`CLK_PERIOD
+    return  <= 4'd3;
+    #`CLK_PERIOD
+    return  <= 4'd4;
+    #`CLK_PERIOD   
+        return  <= 4'd0;
+    #`CLK_PERIOD
+    return  <= 4'd1;
+    #`CLK_PERIOD
+    return  <= 4'd2;
+    #`CLK_PERIOD
+    return  <= 4'd3;
+    #`CLK_PERIOD
+    return  <= 4'd4;
+    #`CLK_PERIOD   
+        return  <= 4'd0;
+    #`CLK_PERIOD
+    return  <= 4'd1;
+    #`CLK_PERIOD
+    return  <= 4'd2;
+    #`CLK_PERIOD
+    return  <= 4'd3;
+    #`CLK_PERIOD
+    return  <= 4'd4;
+    #`CLK_PERIOD   
+    
+    
+    
     return  <= 4'd8;
     subtract <= 1'b0;
     #`CLK_PERIOD
