61db7455d1173483172ade53401d9eab2df3b9bd
Jonas Bertels <r0666113@pc-klas3-10.esat.kuleuven.be>
2020-02-17 14:34:12 +0100

    Added expected and error and result_ok

--- rtl_tb/tb_rsa_wrapper.v
+++ rtl_tb/tb_rsa_wrapper.v
@@ -34,6 +34,8 @@ module tb_rsa_wrapper();
     reg  [1023:0] in_e;
     reg  [1023:0] in_m;
     reg  [1023:0] in_Rsquaredmodm;
+    reg  [511:0] expected;
+    reg  result_ok;
         
     rsa_wrapper rsa_wrapper(
         .clk                    (clk                    ),
@@ -152,7 +154,7 @@ task send_data_to_hw;
         input_data_a[1023:512] <= 512'h87b21d93a10f35511c8d56264a6f95f0245d8004e0d3557c7ec2b396b4ed3cabda34f88e0c8154e9ffab2761e626a720eef1da7ee31ce6c31fcdeaec38eb9589;
         input_data_a <= 512'h901702c94e8d7f3c733aafa46a6b43948148fd2f08761b134bc6815c3a69f4fc4ca4cbec55a2e1e70178549683bf79db5fec9631717e6ae69a5ea5c9eb2a118d;
         input_data_m <= 512'hf8f635bfae6507fc726853e48b8ff18f8037f58fbef63debba0381f2a7da936679f14a270b1129a730d905d283459a275b4dd75470965dfa6386b5321563997d;
-
+        expected     <=  512'hbdb2a4a461dbff5011756139d13f5446a7eb6c9979b55e8fa687b6edaa842d502fc159a825fe144175f9b5616000e5c971e67f150f5135dd5d6fd220f7400189;
 
 
         in_Rsquaredmodm            <=  512'h733f6233b70f1ff7bc7ea9a38d69c2d083bec7c1d73000a3c36a6b4699300aff43a2c4da76786ac6878e16ad896b861ad351008baa901886630148792eca57ad;
@@ -161,7 +163,7 @@ task send_data_to_hw;
         in_e            <=  512'haf;
         
         in_m            <=  512'hd97a21880ab3b85681ef6162732ffcd3cf303982004568f7fba23d0d411ced4080fd567efcd793b308936f7522ead3c53ad80440edd50088935d2a3d9b9c5885;
-               
+                
         
         in_e[1023:512] <=  512'h2685de77f54c47a97e109e9d8cd0032c30cfc67dffba9708045dc2f2bee312bf7f02a98103286c4cf76c908add152c3ac527fbbf122aff776ca2d5c26463a77b;
              
@@ -237,8 +239,12 @@ $display("Test exponentiation ");
         
                 
                 //// --- Print the array contents
-        
-                $display("Output is      %h", output_data_exp);        
+                $display("Output is      %h", output_data_exp[511:0]);        
+                $display("result expected  =%x", expected);
+                $display("error            =%x", expected-output_data_exp[511:0]);
+                    
+                result_ok = (expected==output_data_exp[511:0]);
+                $display("result OK?       =%x", result_ok);      
 
         ///////////////////// END EXAMPLE  /////////////////////  
         
